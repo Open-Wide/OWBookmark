f99ca1567f2acc2f8f57180d1aa5fe43dcbe41c3
Fabien Jarnet <fabien.jarnet@openwide.fr>
2014-12-23 10:37:08 +0100

    init translations

---
+++ translations/fre-FR/translation.ts
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr_FR">
+	<context>
+		<name>context/name/string</name>
+		<message>
+			<source>src string</source>
+			<translation>translated string</translation>
+		</message>
+	</context>
+</TS>
